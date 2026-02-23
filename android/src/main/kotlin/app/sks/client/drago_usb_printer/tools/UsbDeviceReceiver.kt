2b9364dc088e1b6fae62a5b47e3ca75f46443e55
Manish Chauhan <manishchauhan.pms@gmail.com>
2026-02-24 01:14:13 +0530

    Import ContextCompat in UsbDeviceReceiver.kt
    Added import for ContextCompat to UsbDeviceReceiver.kt

--- android/src/main/kotlin/app/sks/client/drago_usb_printer/tools/UsbDeviceReceiver.kt
+++ android/src/main/kotlin/app/sks/client/drago_usb_printer/tools/UsbDeviceReceiver.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
+import androidx.core.content.ContextCompat
 
 /**
  * @Description:    监听USB插拔、USB设备授权
